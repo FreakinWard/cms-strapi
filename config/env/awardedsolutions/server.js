9b94fb508d68f65f2cba2fb42e81e189075cd257
AyAyRon <aaron.l.ward@gmail.com>
2024-03-25 13:06:47 -0500

    build: pm2 ports

--- config/env/awardedsolutions/server.js
+++ config/env/awardedsolutions/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('HOST_PORT_AWARDEDSOLUTIONS', 5200),
+  port: env.int('HOST_PORT_AWARDEDSOLUTIONS', 5300),
   app: {
     keys: env.array('APP_KEYS'),
   },
