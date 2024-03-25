d545f57022f520f2c43fd152b7e6de72c0831259
freakinward <aaron.l.ward@gmail.com>
2024-03-25 16:31:39 -0500

    feat: directedsuccess will now connect to azure mysql

--- config/env/directedsuccess/database.js
+++ config/env/directedsuccess/database.js
@@ -1,33 +1,33 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'mysql',
-//     connection: {
-//       host: env('DATABASE_HOST_DIRECTEDSUCCESS', 'localhost'),
-//       port: env.int('DATABASE_PORT', 3306),
-//       database: env('DATABASE_NAME_DIRECTEDSUCCESS', 'directedsuccess'),
-//       user: env('DATABASE_USERNAME_DIRECTEDSUCCESS', 'root'),
-//       password: env('DATABASE_PASSWORD_DIRECTEDSUCCESS', 'root'),
-//       ssl: env.bool('DATABASE_SSL_DIRECTEDSUCCESS', false),
-//     },
-//   },
-// });
-
-const path = require('path');
-
-module.exports = ({ env }) => {
-
-  const filename = path.join(__dirname, '..', env('DATABASE_FILENAME_DIRECTEDSUCCESS', '.tmp/data-directedsuccess.db'));
-
-  console.log('test', { filename })
-  
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
     connection: {
-      client: 'sqlite',
-      connection: {
-        // filename: path.join(__dirname, '..', env('DATABASE_FILENAME_DIRECTEDSUCCESS', '.tmp/data.db')),
-        filename,
-      },
-      useNullAsDefault: true,
+      host: env('DATABASE_HOST_DIRECTEDSUCCESS', 'empty-host'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME_DIRECTEDSUCCESS', 'empty-database'),
+      user: env('DATABASE_USERNAME_DIRECTEDSUCCESS', 'empty-username'),
+      password: env('DATABASE_PASSWORD_DIRECTEDSUCCESS', 'empty-password'),
+      ssl: env.bool('DATABASE_SSL_DIRECTEDSUCCESS', true),
     },
-  }
-};
+  },
+});
+
+// const path = require('path');
+//
+// module.exports = ({ env }) => {
+//
+//   const filename = path.join(__dirname, '..', env('DATABASE_FILENAME_DIRECTEDSUCCESS', '.tmp/data-directedsuccess.db'));
+//
+//   console.log('test', { filename })
+//
+//   return {
+//     connection: {
+//       client: 'sqlite',
+//       connection: {
+//         // filename: path.join(__dirname, '..', env('DATABASE_FILENAME_DIRECTEDSUCCESS', '.tmp/data.db')),
+//         filename,
+//       },
+//       useNullAsDefault: true,
+//     },
+//   }
+// };
