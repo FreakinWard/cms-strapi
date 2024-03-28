a728e6c9ca1ab5f18e21f76d2c24e9451fba5ba8
freakinward <aaron.l.ward@gmail.com>
2024-03-27 20:09:45 -0500

    refactor: build number

--- src/plugins/health-check/admin/src/components/PluginIcon/index.js
+++ src/plugins/health-check/admin/src/components/PluginIcon/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FaHeartbeat } from "react-icons/fa";
+// import { FaHeartbeat } from "react-icons/fa";
+import { Puzzle } from '@strapi/icons';
 
-const PluginIcon = () => <FaHeartbeat />;
+// const PluginIcon = () => <FaHeartbeat />;
+const PluginIcon = () => <Puzzle />;
 
 export default PluginIcon;
