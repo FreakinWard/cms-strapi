e7c34508f086ef35fc42889bf17bc163329323d4
freakinward <aaron.l.ward@gmail.com>
2024-08-31 21:54:18 -0500

    pending

--- src/plugins/health-check/admin/src/components/HealthMetrics/index.js
+++ src/plugins/health-check/admin/src/components/HealthMetrics/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { Link } from '@strapi/design-system/v2';
 import { Typography } from '@strapi/design-system/Typography';
 
+const LabelLinkItem = metric => <li >
+  <Typography>
+    {`${metric.name}: `}
+    <Link isExternal href={metric.url}>
+      {metric.value}
+    </Link>
+  </Typography>
+
+</li>
+
 export default function Index({ metrics }) {
   return (
       <ul>
         {metrics.map((metric, idx) => {
           if (metric.hasOwnProperty('url')) return (
-              <li key={idx}>
-                <Typography>
-                  {`${metric.name}: `}
-                  <Link isExternal href={metric.url}>
-                    {metric.value}
-                  </Link>
-                </Typography>
-
-              </li>
+              <LabelLinkItem key={idx} metric={metric}/>
+              // <li key={idx}>
+              //   <Typography>
+              //     {`${metric.name}: `}
+              //     <Link isExternal href={metric.url}>
+              //       {metric.value}
+              //     </Link>
+              //   </Typography>
+              //
+              // </li>
           )
 
           return (
