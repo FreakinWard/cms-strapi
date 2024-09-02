a8a2b92f6813d0029f946ef6df4471a05af80ae8
freakinward <aaron.l.ward@gmail.com>
2024-09-01 23:27:11 -0500

    refactor: health plugin breakdown

--- src/plugins/health-check/admin/src/components/HealthMetrics/index.js
+++ src/plugins/health-check/admin/src/components/HealthMetrics/index.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import { Link } from '@strapi/design-system/v2';
-import { Typography } from '@strapi/design-system/Typography';
+import React from "react";
+import { Link } from "@strapi/design-system/v2";
+import { Typography } from "@strapi/design-system/Typography";
 
-const LabelLinkItem = metric => <li >
-  <Typography>
-    {`${metric.name}: `}
-    <Link isExternal href={metric.url}>
-      {metric.value}
-    </Link>
-  </Typography>
+const LabelLinkItem = ({ metric }) => (
+  <li>
+    <Typography>
+      {`${metric.name}: `}
+      <Link isExternal href={metric.url}>
+        {metric.value}
+      </Link>
+    </Typography>
+  </li>
+);
 
-</li>
+const LabelItem = ({ metric }) => (
+  <li>
+    <Typography>{`${metric.name}: ${metric.value}`}</Typography>
+  </li>
+);
+
+const MetricLabel = ({ metric }) => {
+  return metric.hasOwnProperty("url") ? (
+    <LabelLinkItem metric={metric} />
+  ) : (
+    <LabelItem metric={metric} />
+  );
+};
 
 export default function Index({ metrics }) {
   return (
-      <ul>
-        {metrics.map((metric, idx) => {
-          if (metric.hasOwnProperty('url')) return (
-              <LabelLinkItem key={idx} metric={metric}/>
-              // <li key={idx}>
-              //   <Typography>
-              //     {`${metric.name}: `}
-              //     <Link isExternal href={metric.url}>
-              //       {metric.value}
-              //     </Link>
-              //   </Typography>
-              //
-              // </li>
-          )
-
-          return (
-              <li key={idx}>
-                <Typography>{`${metric.name}: ${metric.value}`}</Typography>
-              </li>
-          )
-        })}
-      </ul>
+    <ul>
+      {metrics.map((metric, idx) => (
+        <MetricLabel key={idx} metric={metric} />
+      ))}
+    </ul>
   );
 }
